c83cf50c1fc2562029011b7ccffd84c6aed50981
Pon Cheol Ku (구본철) <dev@bbon.kr>
2021-03-06 19:53:00 +0900

    #23 Add scheduled job

---
+++ Bing.Wallpaper/Controllers/LogsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+using Bing.Wallpaper.Repositories;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Bing.Wallpaper.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Area("api")]
+    [Route("[area]/v{version:apiVersion}/[controller]")]
+    public class LogsController : ApiControllerBase
+    {
+        public LogsController(IAppLogRepository repository, ILoggerFactory loggerFactory)
+        {
+            this.repository = repository;
+            this.logger = loggerFactory.CreateLogger<ImagesController>();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync(int page = 1, int take = 10, string level = "", string keyword = "")
+        {
+            try
+            {
+                var records = await repository.GetAllAsync(page, take, level, keyword);
+
+                logger.LogInformation($"{nameof(ImagesController)}.{nameof(GetAllAsync)} posts.count={records.Count():n0}");
+
+                return StatusCode((int)HttpStatusCode.OK, records);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        private readonly IAppLogRepository repository;
+        private readonly ILogger logger;
+    }
+}
---
+++ Bing.Wallpaper/Repositories/AppLogRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using AutoMapper;
+
+using Bing.Wallpaper.Data;
+using Bing.Wallpaper.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Bing.Wallpaper.Repositories
+{
+    public interface IAppLogRepository
+    {
+        Task<IEnumerable<LogModel>> GetAllAsync(int page = 1, int take = 10,string level = "", string keyword = "");
+    }
+    
+    public class AppLogRepository : IAppLogRepository
+    {
+        public AppLogRepository(DefaultDatabaseContext dbContext, IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.mapper = mapper;
+        }
+
+        public async Task<IEnumerable<LogModel>> GetAllAsync(int page = 1, int take = 10,string level = "", string keyword = "")
+        {
+            var skip = (page - 1) * take;
+            var query = dbContext.Logs.Where(x => true);
+
+            if (!string.IsNullOrWhiteSpace(keyword))
+            {
+                query = query.Where(x => EF.Functions.Like(x.Message, $"%{keyword}%"));
+            }
+
+            if (!string.IsNullOrWhiteSpace(level))
+            {
+                query = query.Where(x => x.Level == level);
+            }
+
+            query = query.OrderByDescending(x => x.Logged).Skip(skip).Take(take);
+
+            var items = await query.Select(x => mapper.Map<LogModel>(x)).AsNoTracking().ToListAsync();
+
+            return items;
+        }
+
+        private readonly DefaultDatabaseContext dbContext;
+        private readonly IMapper mapper;
+    }
+}
